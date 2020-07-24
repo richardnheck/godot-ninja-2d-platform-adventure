4ba5bfe002a61d8e7a382087e94ea330c72385ff
Richard Heck <richardnheck@gmail.com>
2020-07-25 00:23:26 +1000

    Changed player to black outline.  Added animated torch

--- src/objects/KamonKey.gd
+++ src/objects/KamonKey.gd
@@ -3,13 +3,14 @@ extends AnimatedSprite
 signal captured
 
 onready var audioPlayer = $AudioStreamPlayer2D
+onready var collisionShape = $Area2D/CollisionShape2D
 
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body.is_in_group(Constants.GROUP_PLAYER):
 		# The player has captured the key
+		collisionShape.set_deferred("disabled", true)
 		emit_signal("captured")
 		audioPlayer.play()
-		set_physics_process(false)
 		visible = false
 		yield(audioPlayer, "finished")
 		queue_free()		
