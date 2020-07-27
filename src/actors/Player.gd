1c2486b28690dd27154949349e9de9e5f56d58a0
Richard Heck <richardnheck@gmail.com>
2020-07-28 00:16:55 +1000

    Finally added wall jump by holding down jump.  Added rectangular pixel mobile buttons

--- src/actors/Player.gd
+++ src/actors/Player.gd
@@ -27,6 +27,7 @@ onready var die_sound: = $AudioStreamDie
 onready var hit_sound: = $AudioStreamHit
 onready var collision_shape = $CollisionShape2D
 onready var sprite = $AnimatedSprite
+onready var wallJumpCoolDownTimer = $WallJumpCoolDownTimer 
 
 #-----------------------------
 # Variables
@@ -40,6 +41,7 @@ var snap_vector = Constants.SNAP_DIRECTION * Constants.SNAP_LENGTH
 # Double jump
 var double_jump_enabled = false
 var jumps_left = 2			
+var wall_jumping = false
 
 # Dash
 var dash_direction = Vector2(1,0)
@@ -65,7 +67,7 @@ func _physics_process(delta: float) -> void:
 		if collision:
 			emit_signal('collided', collision)
 	
-
+	
 func run(delta):
 	if Input.is_action_pressed(Actions.MOVE_RIGHT):
 		vel.x += speed
@@ -74,7 +76,44 @@ func run(delta):
 		vel.x -= speed
 		#sprite.flip_h = true
 		
+
+
+# Jump and wall jump by holding down jump and pressing left or right on wall to jump
 func jump():
+	var dir = get_direction()
+	
+	# I can jump when I'm on floor or next to the wall
+	if is_on_floor() or next_to_wall():
+		jumps_left = 2 if double_jump_enabled else 1
+		
+	if Input.is_action_just_pressed(Actions.JUMP) and jumps_left > 0:
+		print("jump pressed")
+		if vel.y > 0: vel.y = 0 # if I'm falling - ignore fall velocity
+
+		if is_on_floor():
+			# Normal jump
+			vel.y -= jump_power
+	
+	# Handle wall jump
+	if Input.is_action_pressed(Actions.JUMP):
+		# Wall jump occurs when user holds down jump
+		if not wall_jumping:
+			if (next_to_left_wall() and (dir.x == 1)) or (next_to_right_wall() and (dir.x == -1)):
+				wall_jump(dir)				
+	
+	jumps_left -= 1				#	
+		
+	# If I'm still going up and have released the jump button - cut off the jump and start falling down
+	if Input.is_action_just_released(Actions.JUMP) and vel.y < 0:
+		vel.y = 0
+
+func wall_jump(dir):
+	wallJumpCoolDownTimer.start()
+	wall_jumping = true
+	vel.y= -wall_jump_power
+	vel.x+= dir.x * 100	
+
+func old_jump(delta):
 	var dir = get_direction()
 	
 	# I can jump when I'm on floor or next to the wall
@@ -87,17 +126,22 @@ func jump():
 		var pressing_against_wall = (next_to_left_wall() and dir.x < 0) or (next_to_right_wall() and dir.x > 0)
 		#if is_on_floor() or pressing_against_wall:
 			# not next to a wall so jump normally
-		print(vel.x)
+		
 		if next_to_wall(): 
 			if is_on_floor():
 				# normal jump
 				vel.y -= jump_power
 			else:
-				if (next_to_left_wall() and abs(vel.x) < 30)  or (next_to_right_wall() and abs(vel.x) < 30):
-					vel.y -= 200	
-				else:
-					vel.y -= wall_jump_power				
-			
+				# Wall jump if not pressing against wall and pressing a direction
+				# This means you can't just jump vertically up the wall				
+				if !pressing_against_wall and abs(vel.x) > 0:
+					vel.y -= wall_jump_power	
+				
+#				if (next_to_left_wall() and abs(vel.x) < 30)  or (next_to_right_wall() and abs(vel.x) < 30):
+#					vel.y -= 200	
+#				else:
+#					vel.y -= wall_jump_power				
+
 		else:
 			# normal jump
 			vel.y -= jump_power
@@ -105,10 +149,14 @@ func jump():
 		jumps_left -= 1		
 		
 		# Jump away from the wall
-		if not is_on_floor() and next_to_left_wall():
-			vel.x += wall_jump_speed
-		if not is_on_floor() and next_to_right_wall():
-			vel.x -= wall_jump_speed
+#		if not is_on_floor() and next_to_left_wall():
+#			#vel.x += wall_jump_speed
+#			#vel.x = clamp(vel.x, 125, 225)
+#		if not is_on_floor() and next_to_right_wall():
+#			vel.x -= wall_jump_speed
+#			#vel.x = clamp(vel.x, -225, -125)
+		
+		print(vel.x)
 	
 	# If I'm still going up and have released the jump button - cut off the jump and start falling down
 	if Input.is_action_just_released(Actions.JUMP) and vel.y < 0:
@@ -129,14 +177,14 @@ func gravity():
 	if vel.y > 500: 
 		vel.y = 500 # clamp falling speed
 	
-	# Wall slide if next to left wall and player is pressing left
-	if next_to_left_wall() and get_direction().x < 0 and vel.y > wall_slide_speed:
-		vel.y = wall_slide_speed 
-		
-	# Wall slide if next to right wall and player is pressing right
-	if next_to_right_wall() and get_direction().x > 0 and vel.y > wall_slide_speed:
-		vel.y = wall_slide_speed 
-	#if next_to_wall() and vel.y > 100: 
+#	# Wall slide if next to left wall and player is pressing left
+#	if next_to_left_wall() and get_direction().x < 0 and vel.y > wall_slide_speed:
+#		vel.y = wall_slide_speed 
+#
+#	# Wall slide if next to right wall and player is pressing right
+#	if next_to_right_wall() and get_direction().x > 0 and vel.y > wall_slide_speed:
+#		vel.y = wall_slide_speed 
+#	#if next_to_wall() and vel.y > 100: 
 		
 
 
@@ -200,6 +248,7 @@ func die():
 func celebrate():
 	$AnimatedSprite.play("celebrate")
 
+
 func next_to_wall():
 	return next_to_left_wall() or next_to_right_wall()
 
@@ -217,4 +266,12 @@ func _on_EnemyDetector_area_entered(area: Area2D) -> void:
 
 	
 func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+	die()
+
+
+func _on_WallJumpCoolDownTimer_timeout() -> void:
+	wall_jumping = false
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
 	die()
