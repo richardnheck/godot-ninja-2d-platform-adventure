19c4f27a546eac9bb5fad69cdc4e4987136989cf
Richard Heck <richardnheck@gmail.com>
2020-09-20 23:02:12 +1000

    Improved functionality and image of checkpoint

--- src/objects/CheckPoint.gd
+++ src/objects/CheckPoint.gd
@@ -2,7 +2,15 @@ extends Area2D
 
 signal reached
 
+var on:bool = false
+
+func _ready() -> void:
+	on = false
+	$AnimatedSprite.play("off")
+
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group(Constants.GROUP_PLAYER):
+	if body.is_in_group(Constants.GROUP_PLAYER) and !on:
+		on = true
+		$AnimatedSprite.play("on")
 		$Sound.play()
 		emit_signal("reached")
