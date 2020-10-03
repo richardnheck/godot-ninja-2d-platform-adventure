bd0822f83a98a782a130e9ff7a9ac6135e103380
Richard Heck <richardnheck@gmail.com>
2020-10-03 22:24:59 +1000

    Added jump intro stage and converted "Across the Abyss" level to "Go Ahead Jump", which has spikes instead of hole

--- src/settings/LevelData.gd
+++ src/settings/LevelData.gd
@@ -3,8 +3,9 @@ extends Node
 const CAVE_LEVELS_PATH = "res://src/levels/CaveLevels/CaveLevel"
 
 var levelsArray = [
+	CAVE_LEVELS_PATH + "JumpIntro.tscn",
 	CAVE_LEVELS_PATH + "1.tscn",
-	CAVE_LEVELS_PATH + "AcrossTheAbyss.tscn",
+	CAVE_LEVELS_PATH + "GoAheadJump.tscn",
 	CAVE_LEVELS_PATH + "LearnWallJumpImproved.tscn",
 	CAVE_LEVELS_PATH + "LearnCrumblingRocks.tscn",
 	CAVE_LEVELS_PATH + "CrumblingRocksLevel.tscn",
