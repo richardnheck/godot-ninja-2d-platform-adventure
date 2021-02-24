ae65628bf2abdc94a72fe7645827350bbc56a064
Richard Heck <richardnheck@gmail.com>
2021-02-24 22:25:45 +1100

    experimented with rigidbody for boss in cutscene

--- src/UI/CutScenes/CaveLevel/BossClearCutScene.gd
+++ src/UI/CutScenes/CaveLevel/BossClearCutScene.gd
@@ -1,6 +1,7 @@
 extends Node
 
 onready var player:Player = $Player
+onready var boss:RigidBody2D = $BossRigidBody
 onready var animation_player:AnimationPlayer = $AnimationPlayer
 
 var _move_right:bool = false
@@ -9,9 +10,12 @@ var _move_left:bool = false
 func _ready() -> void:
 	Actions.use_cutscene_actions()
 	
-	animation_player.play("walk_in")	
-		
-
+	#animation_player.play("walk_in")	
+	yield(get_tree().create_timer(1), "timeout")
+	$Sprite.position = boss.get_global_transform().origin
+	boss.apply_impulse(boss.get_global_transform().origin, Vector2(100,-350))
+	#boss.apply_central_impulse(Vector2(0,500))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if _move_right:
