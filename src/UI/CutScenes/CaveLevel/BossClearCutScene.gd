318d1f4b8f9fc13bbfb355220433723130d60771
Richard Heck <richardnheck@gmail.com>
2021-02-25 22:32:18 +1100

    more work on boss clear scene

--- src/UI/CutScenes/CaveLevel/BossClearCutScene.gd
+++ src/UI/CutScenes/CaveLevel/BossClearCutScene.gd
@@ -1,20 +1,26 @@
 extends Node
 
 onready var player:Player = $Player
-onready var boss:RigidBody2D = $BossRigidBody
+onready var boss:RigidBody2D = $Boss
 onready var animation_player:AnimationPlayer = $AnimationPlayer
 
 var _move_right:bool = false
 var _move_left:bool = false
 
 func _ready() -> void:
+	
+	
 	Actions.use_cutscene_actions()
 	
-	#animation_player.play("walk_in")	
-	yield(get_tree().create_timer(1), "timeout")
-	$Sprite.position = boss.get_global_transform().origin
-	boss.apply_impulse(boss.get_global_transform().origin, Vector2(100,-350))
-	#boss.apply_central_impulse(Vector2(0,500))
+	animation_player.play("walk_in")	
+	#do_boss_walk_in()#yield(get_tree().create_timer(1), "timeout")
+
+func _physics_process(delta: float) -> void:
+	if set_pos:
+		boss.set_global_position(boss.position)
+		set_pos = false
+	
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -24,6 +30,18 @@ func _process(delta: float) -> void:
 		player.move_left()
 	else:
 		player.move_stop()
+
+func do_boss_walk_in() -> void:
+	animation_player.play("boss_walk_in")
+
+var set_pos = false
+
+func do_boss_jump() -> void:
+	print("boss jump")
+	print(boss.position)
+	boss.set_global_position(boss.position)
+	boss.apply_central_impulse( Vector2(100,-350))
+	
 
 func move_right() -> void:
 	_move_right = true
@@ -38,7 +56,10 @@ func move_stop() -> void:
 	_move_right = false
 	
 func look_left() -> void:
-	print("look left")
 	move_left()
 	yield(get_tree().create_timer(0.05), "timeout")
 	move_stop()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	print(anim_name + " animation finished")
