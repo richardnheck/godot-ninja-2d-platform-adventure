53189b914b30417ae030dbe1176cc49934002082
Richard Heck <richardnheck@gmail.com>
2021-02-26 22:33:26 +1100

    worked on boss clear scene

--- src/UI/CutScenes/CaveLevel/BossClearCutScene.gd
+++ src/UI/CutScenes/CaveLevel/BossClearCutScene.gd
@@ -3,63 +3,74 @@ extends Node
 onready var player:Player = $Player
 onready var boss:RigidBody2D = $Boss
 onready var animation_player:AnimationPlayer = $AnimationPlayer
+onready var screen_shake = $ScreenShake
 
-var _move_right:bool = false
-var _move_left:bool = false
+var _move_player_right:bool = false
+var _move_player_left:bool = false
+var _move_boss_right:bool = false
+
 
 func _ready() -> void:
-	
-	
 	Actions.use_cutscene_actions()
+	screen_shake.set_camera_node("Camera2D")
 	
 	animation_player.play("walk_in")	
-	#do_boss_walk_in()#yield(get_tree().create_timer(1), "timeout")
-
-func _physics_process(delta: float) -> void:
-	if set_pos:
-		boss.set_global_position(boss.position)
-		set_pos = false
-	
 	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if _move_right:
+	if _move_player_right:
 		player.move_right()
-	elif _move_left:
+	elif _move_player_left:
 		player.move_left()
 	else:
 		player.move_stop()
-
+		
+	if _move_boss_right:
+		boss.linear_velocity = Vector2(90,0)
+	
+		
 func do_boss_walk_in() -> void:
 	animation_player.play("boss_walk_in")
-
-var set_pos = false
-
+	
 func do_boss_jump() -> void:
-	print("boss jump")
-	print(boss.position)
-	boss.set_global_position(boss.position)
-	boss.apply_central_impulse( Vector2(100,-350))
+	move_boss_stop()
+	boss.apply_central_impulse( Vector2(45,-200))
+	yield(get_tree().create_timer(4), "timeout")
+	boss.queue_free()
+	do_ending()
 	
+func do_ending() -> void:
+	screen_shake.screen_shake(1,4,100)		
+	yield(get_tree().create_timer(1), "timeout")
+	player.celebrate()
+		
 
-func move_right() -> void:
-	_move_right = true
-	_move_left = false
+func move_player_right() -> void:
+	_move_player_right = true
+	_move_player_left = false
 	
-func move_left() -> void:
-	_move_left = true
-	_move_right = false
+func move_player_left() -> void:
+	_move_player_left = true
+	_move_player_right = false
 	
-func move_stop() -> void:
-	_move_left = false
-	_move_right = false
+func move_player_stop() -> void:
+	_move_player_left = false
+	_move_player_right = false
 	
-func look_left() -> void:
-	move_left()
+func look_player_left() -> void:
+	move_player_left()
 	yield(get_tree().create_timer(0.05), "timeout")
-	move_stop()
+	move_player_stop()
 
+
+
+func move_boss_right() -> void:
+	_move_boss_right = true
+
+func move_boss_stop() -> void:
+	boss.linear_velocity = Vector2(0,0)
+	_move_boss_right = false
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	print(anim_name + " animation finished")
