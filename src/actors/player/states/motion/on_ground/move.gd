df9b704f79491986ff497e4879b84ff92fb21fe0
Richard Heck <richardnheck@gmail.com>
2021-04-20 22:28:12 +1000

    experiment with particle effects for moving (doesn't work)

--- src/actors/player/states/motion/on_ground/move.gd
+++ src/actors/player/states/motion/on_ground/move.gd
@@ -4,10 +4,13 @@ extends "on_ground.gd"
 #export(float) var max_run_speed = 700
 var just_landed = false
 var on_ground = true
+var particles_move:CPUParticles2D = null
+var input_direction = 1
 
 func enter():
 	.enter()
 	just_landed = false
+	particles_move = owner.get_node("ParticlesMove")
 	
 	speed = 0.0
 	velocity = Vector2()
@@ -15,6 +18,9 @@ func enter():
 	var input_direction = get_input_direction()
 	update_look_direction(input_direction)
 	owner.get_node("AnimatedSprite").play("run")
+
+func exit():
+	_show_move_particles(false)
 
 func initialize(_jumpPressedRemember):
 	jumpPressedRemember = _jumpPressedRemember
@@ -25,7 +31,7 @@ func handle_input(event):
 
 
 func update(_delta):
-	var input_direction = get_input_direction()
+	input_direction = get_input_direction()
 	
 	update_look_direction(input_direction)
 	apply_gravity()
@@ -42,16 +48,29 @@ func update(_delta):
 			just_landed = false
 			
 		owner.get_node("AnimatedSprite").play("run")
+		
+		_show_move_particles(true)
 	else:
 		on_ground = false
 		owner.get_node("AnimatedSprite").play("jump_down")
+		
 #	
 	if just_landed:
 		owner.do_landing()	
 		
 		
 	if owner.is_on_floor() and !input_direction:
+		_show_move_particles(false)
 		emit_signal("finished", "idle")
 		
 	detect_and_transition_to_jump(_delta)
 	detect_and_transition_to_wall_slide()
+
+func _show_move_particles(show:bool):
+	if show:
+		particles_move.emitting = true
+		particles_move.scale.x = input_direction.x
+		particles_move.position.x = -6 * input_direction.x
+	else:
+		particles_move.emitting = false
+		particles_move.restart()
