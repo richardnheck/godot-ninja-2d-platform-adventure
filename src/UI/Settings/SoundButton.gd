bc1a58755c9be242204bd303ea04988d35dc0f68
Richard Heck <richardnheck@gmail.com>
2021-05-24 23:09:52 +1000

    trying to get settings buttons working using ui themes

---
+++ src/UI/Settings/SoundButton.gd
@@ -0,0 +1,29 @@
+extends Button
+
+onready var icon_atlas = preload("res://assets/themes/settings/settings-button-icons.png")
+var muted_icon_texture:AtlasTexture
+var normal_icon_texture:AtlasTexture
+
+
+func _ready() -> void:
+	normal_icon_texture = AtlasTexture.new()
+	normal_icon_texture.atlas = icon_atlas
+	normal_icon_texture.region = Rect2(0,0,14,14)
+	
+	muted_icon_texture = AtlasTexture.new()
+	muted_icon_texture.atlas = icon_atlas
+	muted_icon_texture.region = Rect2(14,0,14,14) 
+	
+	_update_icon()
+
+
+func _on_button_up() -> void:
+	Game_AudioManager.sfx_ui_basic_blip_select.play()
+	Game_AudioManager.toggle_sound_fx()
+	_update_icon()
+
+func _update_icon():
+	if not Game_AudioManager.is_sound_fx_muted():
+		self.icon = normal_icon_texture
+	else:
+		self.icon = muted_icon_texture
