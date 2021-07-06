60e499d90ba6b135e1bc2ee26af27d6a120aa529
Richard Heck <richardnheck@gmail.com>
2021-07-06 22:45:59 +1000

    Experimenting with play button on main screen

--- src/UI/MainScreen/MainScreen.gd
+++ src/UI/MainScreen/MainScreen.gd
@@ -1,21 +1,29 @@
 extends CanvasLayer
 
+onready var title = $TitleScreenText
+onready var title_tween = $TitleTween
+onready var tween_values = [null, null]
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Settings.visible = false
 	Game_AudioManager.play_bgm_main_theme_skip_start()
 
+	_start_tween()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _start_tween():
+	if tween_values[0] == null:
+		tween_values = [title.global_position, Vector2(title.global_position.x, title.global_position.y - 8)]
+	title_tween.interpolate_property(title, "position", tween_values[0], tween_values[1], 2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	title_tween.start()
 
 
 func _on_SettingsButton_pressed() -> void:
 	Game_AudioManager.sfx_ui_basic_blip_select.play()
 	$Settings.visible = true 
+
+
+func _on_TitleTween_tween_completed(object: Object, key: NodePath) -> void:
+	tween_values.invert()
+	_start_tween()
