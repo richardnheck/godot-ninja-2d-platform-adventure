c1483234de31382be3db574a2c7b4ea08ca64349
Richard Heck <richardnheck@gmail.com>
2021-07-15 22:51:06 +1000

    Fire yokai now flies off when triggered

--- src/objects/falling-spike/FallingSpike.gd
+++ src/objects/falling-spike/FallingSpike.gd
@@ -31,14 +31,16 @@ func _physics_process(delta: float) -> void:
 				yield(animatedSprite, "animation_finished")
 				queue_free()
 
+
 func trigger() -> void:
+	# Trigger the fire yokai to fly off
+	if is_instance_valid(fire_yokai): 
+		fire_yokai.trigger()
+	
+	# Shake the spike
 	animationPlayer.play("shake")
 	yield(animationPlayer,"animation_finished")
 	
-	fire_yokai = preload("res://src/objects/fire-yokai/FireYokai.tscn").instance()
-	fire_yokai.global_position = global_position
-	get_parent().add_child(fire_yokai)		
-
 	Game_AudioManager.sfx_env_falling_spike.play()
 	triggered = true	
 	
@@ -51,3 +53,12 @@ func _on_HitZone_body_entered(body: Node) -> void:
 	if body.is_in_group(Constants.GROUP_PLAYER):
 		print("HIT!!!")
 		body.die()
+
+func _on_AddFireYokaiTimer_timeout() -> void:
+	_add_fire_yokai()
+
+func _add_fire_yokai() -> void:
+	fire_yokai = preload("res://src/objects/fire-yokai/FireYokai.tscn").instance()
+	fire_yokai.global_position = Vector2(global_position.x, global_position.y - 12)
+	get_parent().add_child(fire_yokai)		
+	
--- src/objects/fire-yokai/FireYokai.gd
+++ src/objects/fire-yokai/FireYokai.gd
@@ -5,13 +5,12 @@ class_name FireYokai
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 func trigger():
+	# Fly it off the screen upwards
 	var sprite = self
-	$Tween.interpolate_property(self, "position", sprite.global_position, Vector2(sprite.global_position.x, sprite.global_position.y - 100), 1.5, Tween.TRANS_QUINT, Tween.EASE_IN)    
+	$Tween.interpolate_property(self, "position", sprite.global_position, Vector2(sprite.global_position.x, sprite.global_position.y - 100), 0.5, Tween.TRANS_BACK, Tween.EASE_IN)    
 	$Tween.start()
 
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _on_Tween_tween_completed(object: Object, key: NodePath) -> void:
+	# When tween ends remove object from scene
+	queue_free()
