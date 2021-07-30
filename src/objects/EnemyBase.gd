b85cf262d37aa45586f5e6bf3779093210cf8b67
Richard Heck <richardnheck@gmail.com>
2021-07-30 22:34:41 +1000

    improved spikey soot yokai with shading.  now looks at player.  Lightened background on all levels

--- src/objects/EnemyBase.gd
+++ src/objects/EnemyBase.gd
@@ -1,8 +1,10 @@
 extends Node
 
+export var look_at_player_enabled:bool = true
 
 onready var area2D:Area2D = $Area2D
 onready var animatedSprite:AnimatedSprite = $AnimatedSprite
+onready var player:Player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,8 +15,22 @@ func _ready() -> void:
 	var t = rand_range(0,2)
 	yield(get_tree().create_timer(t),"timeout")
 	animatedSprite.playing = true
+	
+	_find_player()
+
+func _process(delta: float) -> void:
+	if look_at_player_enabled:
+		_look_at_player()
+	
 
 # Handle when a body enters the object
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group(Constants.GROUP_PLAYER):
 		body.die()
+
+func _find_player() -> void:
+	player = get_tree().get_nodes_in_group("player")[0]
+
+func _look_at_player() -> void:
+	if player:
+		animatedSprite.flip_h = player.global_position < self.global_position
