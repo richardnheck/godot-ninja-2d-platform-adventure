f333642e0f283ea3c4f5bf3a82a5b8f880ad7d2c
Richard Heck <richardnheck@gmail.com>
2022-07-24 22:11:58 +1000

    Fixed fireball aiming so it is more accurate in phase 2 and finised the first pass in boss level

--- src/characters/enemies/Wanyudo/NormalFireball.gd
+++ src/characters/enemies/Wanyudo/NormalFireball.gd
@@ -13,20 +13,24 @@ onready var life_timer = $LifeTimer
 onready var explosion: = $AnimatedSpriteExplosion
 
 var target:Player = null
+
+var player_speed = 0    # horizontal speed
+var speed_factor = 1
 		
 func fire(target_ref:Player):
 	target = target_ref
 	
-	if target:
-		# Get the player velocity so we can aim at the player when they are moving
-		var player_velocity = target.get_node('StateMachine').current_state.velocity
-		var x_offset = player_velocity.x
-		
+	if target:	
+		var x_offset = player_speed
 		direction = position.direction_to(target.position + Vector2(x_offset,0))
 
 var elapsed = 0.0
 func _process(delta):	
 	global_position += speed * delta * direction.normalized()#	
+	
+	var player_velocity = target.get_node('StateMachine').current_state.velocity
+	player_speed = lerp(player_velocity.x, 0, delta * speed_factor) 
+		
 	# Attempt #2 (this actually works quite well but rotation doesn't work easily)
 	#	var follow_speed = 1
 	#	position.y = lerp(position.y, target.position.y, delta * follow_speed ) 
