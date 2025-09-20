175ae6c312c4e2ba386dbf37b7bf26c8195930ae
Richard Heck <richardnheck@gmail.com>
2025-09-20 23:35:33 +1000

    feat: added splash when fish yokai jump out of and return to water

--- src/objects/water-jump-yokai/WaterJumpYokai.gd
+++ src/objects/water-jump-yokai/WaterJumpYokai.gd
@@ -8,6 +8,8 @@ var impulse := 1000.0 setget set_impulse
 var falling := false
 onready var _sprite := $AnimatedSprite
 
+var splash_scene = preload("res://src/objects/water-splash/WaterSplash.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -28,8 +30,10 @@ func _physics_process(delta):
 func _on_body_entered(body: Node) -> void:
 	print("body entered")
 	if body.is_in_group(Constants.GROUP_PLAYER):
-		print("body entered")
+		print("hit player")
 		body.die()
+#	else:
+#		
 
 
 func set_direction(new_direction: Vector2) -> void:
@@ -46,3 +50,15 @@ func _on_Timer_timeout() -> void:
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
+
+# Area2D is used for detection of water trap
+func _on_Area2D_body_entered(body):
+	# Add a splash when leaving and hitting the water
+	var splash_instance = splash_scene.instance()
+	splash_instance.global_position = global_position + Vector2(0,0)
+	get_parent().add_child(splash_instance)
+	
+	if linear_velocity.y > 0:
+		# falling back in to water so remove
+		queue_free()
+	
