3ce9fff4c0f6f88bcce27eed73818ec4b73a09a6
Richard Heck <richardnheck@gmail.com>
2025-10-26 17:07:53 +1100

    feat: added space to jump, added more sections to world3 level2 and tweaked long moving spike to have a delay between cycles and retract spike quicker

--- src/objects/long-moving-spike/LongMovingSpike.gd
+++ src/objects/long-moving-spike/LongMovingSpike.gd
@@ -12,7 +12,16 @@ onready var tween_values = [Vector2(0,0), Vector2(0,-46)]
 
 export var initial_delay:float = 0		# initial delay in seconds
 
-var spike_moving_out = true;
+var delay:float = 0.3	# time delay between cycles
+var spike_moving_out := true;
+
+var first_cycle = true
+
+# The time it takes to extend the spike
+const time_extend := 1.5
+
+# The time it takes to retract the spike
+const time_retract := 1
 
 func _ready():
 	# disable handle area collisions when retracted 
@@ -25,17 +34,25 @@ func _ready():
 
 
 func _start_tween():
-	$Tween.interpolate_property(moving_part, "position", tween_values[0], tween_values[1], 1.5,
+	var time = time_extend if not spike_moving_out else time_retract
+	$Tween.interpolate_property(moving_part, "position", tween_values[0], tween_values[1], time,
 	 Tween.TRANS_QUINT, tween.EASE_IN)    
 	$Tween.start()
 
 
 func _on_tween_completed(object, key):
 	if spike_moving_out:
+		# spike is fully extended
 		sound.play()
+	else:
+		# spike is fully retracted
+		# wait a bit before starting the next cycle
+		yield(get_tree().create_timer(delay), "timeout")
+				
 	tween_values.invert()
 	_start_tween()
 	spike_moving_out = !spike_moving_out
+	first_cycle = false
 
 
 func _on_InitialDelayTimer_timeout() -> void:
