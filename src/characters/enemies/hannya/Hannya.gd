706b58b3ca4f7a7d4b981aaba58dec7ea03b9f52
Richard Heck <richardnheck@gmail.com>
2025-11-05 14:55:13 +1100

    feat: added telegraph to hannya and started another section in World3 Level 4

--- src/characters/enemies/hannya/Hannya.gd
+++ src/characters/enemies/hannya/Hannya.gd
@@ -10,6 +10,13 @@ enum Direction {
 export(Direction) var direction = Direction.RIGHT setget _set_direction
 
 onready var trigger_collision_shape := $TriggerArea2D/CollisionShape2D
+onready var telegraph_tween:= $TelegraphTween
+onready var animated_sprite:=$AnimatedSprite 
+
+var telegraph_tween_values = [Vector2.ZERO, Vector2.ZERO]
+
+# The time in seconds to telegraph attack before flying
+const telegraph_time:= 0.3
 
 # When triggered, the Hannya flies across the screen at high speed
 var triggered = false
@@ -21,8 +28,14 @@ func _set_direction(value) -> void:
 	direction = value
 	
 func _ready() -> void:
+	print_debug("Hannya")
+	print_debug(str(global_position))
+	
 	# set the direction of the Hannya
 	self.scale.x = -direction
+	
+	# set the telegraph tween value to move the sprite backwards before attacking
+	telegraph_tween_values = [Vector2.ZERO, Vector2(direction * 3,0)]
 	
 		
 func _physics_process(delta:float) -> void:
@@ -40,9 +53,18 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 func _on_TriggerArea2D_body_entered(body):
 	if body.is_in_group(Constants.GROUP_PLAYER):
-		triggered = true
+		# Trigger the telegraph before the attack
+		telegraph_tween.interpolate_property(animated_sprite, "offset", telegraph_tween_values[0], telegraph_tween_values[1], telegraph_time, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	
+		telegraph_tween.start()
+		
 
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group(Constants.GROUP_PLAYER):
 		body.die()
+
+
+func _on_TelegraphTween_tween_completed(object, key):
+	# telegraph has finished so trigger the main fast surge attack
+	triggered = true
