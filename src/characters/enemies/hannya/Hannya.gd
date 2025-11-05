7d51835fd1e7bf20c3eb701795433171fcb7286d
Richard Heck <richardnheck@gmail.com>
2025-11-05 16:32:33 +1100

    fix: fixed hannya telegraph positioning and tweaked some test sections in World3 Level4

--- src/characters/enemies/hannya/Hannya.gd
+++ src/characters/enemies/hannya/Hannya.gd
@@ -16,12 +16,15 @@ onready var animated_sprite:=$AnimatedSprite
 var telegraph_tween_values = [Vector2.ZERO, Vector2.ZERO]
 
 # The time in seconds to telegraph attack before flying
-const telegraph_time:= 0.3
+const telegraph_time:= 0.20
+
+# The distance in pixels of the telegraph (i.e. move back before attacking)
+const telegraph_distance:= 10
 
 # When triggered, the Hannya flies across the screen at high speed
 var triggered = false
 
-const speed = 300
+const speed = 390
 var velocity = Vector2()
 
 func _set_direction(value) -> void:
@@ -33,9 +36,6 @@ func _ready() -> void:
 	
 	# set the direction of the Hannya
 	self.scale.x = -direction
-	
-	# set the telegraph tween value to move the sprite backwards before attacking
-	telegraph_tween_values = [Vector2.ZERO, Vector2(direction * 3,0)]
 	
 		
 func _physics_process(delta:float) -> void:
@@ -53,10 +53,12 @@ func _on_VisibilityNotifier2D_screen_exited():
 
 func _on_TriggerArea2D_body_entered(body):
 	if body.is_in_group(Constants.GROUP_PLAYER):
-		# Trigger the telegraph before the attack
-		telegraph_tween.interpolate_property(animated_sprite, "offset", telegraph_tween_values[0], telegraph_tween_values[1], telegraph_time, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
-	
-		telegraph_tween.start()
+		if not triggered:
+			# Trigger the telegraph before the attack
+			# set the telegraph tween value to move the sprite backwards before attacking
+			telegraph_tween_values = [position, Vector2(position.x + (-direction * telegraph_distance), position.y)]
+			telegraph_tween.interpolate_property(self, "position", telegraph_tween_values[0], telegraph_tween_values[1], telegraph_time, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+			telegraph_tween.start()
 		
 
 
