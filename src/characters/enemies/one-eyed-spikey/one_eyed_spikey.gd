6a16db261793ecfbd2d9c47cc53f109c83c921b9
Richard Heck <richardnheck@gmail.com>
2025-12-05 23:06:38 +1100

    feat: one eyed spikey can now move horizontally

--- src/characters/enemies/one-eyed-spikey/one_eyed_spikey.gd
+++ src/characters/enemies/one-eyed-spikey/one_eyed_spikey.gd
@@ -1,7 +1,13 @@
+class_name OneEyedSpikey
 extends SimpleEnemy
 
+enum Direction {
+	UP_DOWN,
+	LEFT_RIGHT
+}
 
 export var float_offset:int = 50
+export (Direction) var direction := Direction.UP_DOWN;
 
 onready var tween_values = [Vector2.ZERO, Vector2.ZERO]
 
@@ -16,7 +22,10 @@ func _ready() -> void:
 # -----------------------------------------------------------------------
 func _start_tween():
 	if(tween_values[0] == Vector2.ZERO):
-		tween_values = [self.global_position, Vector2(self.global_position.x, self.global_position.y - float_offset)]
+		if direction == Direction.UP_DOWN:
+			tween_values = [self.global_position, Vector2(self.global_position.x, self.global_position.y - float_offset)]
+		else:
+			tween_values = [self.global_position, Vector2(self.global_position.x - float_offset, self.global_position.y)]	
 	tween.interpolate_property(self, "position", tween_values[0], tween_values[1], 2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
 	tween.start()	
 
