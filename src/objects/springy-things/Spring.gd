b5265e82d5d48f3b1b5e45f2d5fa2f5e0f1ac159
Richard Heck <richardnheck@gmail.com>
2026-01-01 15:35:32 +1100

    feat: added spring art and animation

--- src/objects/springy-things/Spring.gd
+++ src/objects/springy-things/Spring.gd
@@ -2,11 +2,18 @@ extends Area2D
 
 const impulse = 135
 
+onready var animated_sprite = $AnimatedSprite
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animated_sprite.animation = "default"
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group(Constants.GROUP_PLAYER):
 		if body.has_method("spring"):
 			body.spring(Vector2(0, impulse))
+			animated_sprite.play("launch")
+
+
+func _on_AnimatedSprite_animation_finished():
+	animated_sprite.animation = "default"
