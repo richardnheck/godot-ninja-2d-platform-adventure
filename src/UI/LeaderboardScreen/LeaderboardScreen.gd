5ff3bdcadb9a6e7934fc46069bdb7234b0fbe9be
Richard Heck <richardnheck@gmail.com>
2026-04-10 00:07:59 +1000

    feat: experimented with adding labels to container instead of using itemlist

--- src/UI/LeaderboardScreen/LeaderboardScreen.gd
+++ src/UI/LeaderboardScreen/LeaderboardScreen.gd
@@ -9,7 +9,7 @@ onready var item_list = $ItemList
 
 onready var cut_scene_base:CutSceneBase = $"%CutSceneBase"
 
-var ItemListContent = ["We shall go this way","We shall go that way","which way shall we go?","I think we're lost"]
+onready var list_container = $"%VBoxContainer"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
@@ -17,11 +17,18 @@ func _ready() -> void:
 	pass
 	
 func _load_entries() -> void:
+	var normal_theme = load("res://assets/themes/settings/settings_normal_theme.tres")
+	
 	var entries_page = yield(Analytics.get_level_leaderboard_entries(), "completed")
 	for entry in entries_page.entries:
 		var display_name = entry.player_display_name
 		var label = "%s  %s   %s" % [entry.position + 1, display_name, Stopwatch.get_time_as_formatted_string(entry.score, Stopwatch.TimeFormat)]
 		item_list.add_item(label,null,true)
+		
+		var labelItem:Label = Label.new()
+		labelItem.text = label
+		labelItem.theme = normal_theme
+		list_container.add_child(labelItem)
 	
 	
 func _on_CloseButton_pressed() -> void:
