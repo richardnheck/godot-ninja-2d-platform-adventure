0cc1d8d987ac83c1e53045afbdd7b85db4d60d6d
Richard Heck <richardnheck@gmail.com>
2026-04-25 22:41:01 +1000

    fix: boss2 clear cutscene now uses animated player to be more robust

--- src/UI/CutScenes/World2/BossClearCutScene.gd
+++ src/UI/CutScenes/World2/BossClearCutScene.gd
@@ -4,7 +4,6 @@
 extends Node
 
 onready var cut_scene_base:CutSceneBase = $CutSceneBase
-onready var player:Player = $Player
 onready var boss = $Boss
 onready var animation_player:AnimationPlayer = $AnimationPlayer
 onready var text_animation_player:AnimationPlayer = $TextAnimationPlayer
@@ -12,10 +11,8 @@ onready var screen_shake = $ScreenShake
 onready var stage_clear_text = $StageClearText
 onready var tween = $Tween
 onready var fade_screen = $FadeScreen
-
-var _move_player_right:bool = false
-var _move_player_left:bool = false
-
+onready var demon_seal = $"%DemonSeal"
+onready var player_for_cutscene = $"%PlayerForCutscene"
 
 func _ready() -> void:
 	# Randomize the random number generator's seed
@@ -35,20 +32,8 @@ func _ready() -> void:
 	Actions.use_cutscene_actions()
 	screen_shake.set_camera_node("Camera2D")
 	stage_clear_text.visible = false
-	
-	# temp
+
 	animation_player.play("walk_in")	
-	player.get_camera_manager().get_camera().current = false  # make sure the player's camera is not used
-	
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if _move_player_right:
-		player.move_right()
-	elif _move_player_left:
-		player.move_left()
-	else:
-		player.move_stop()
 
 func play_bgm() -> void:
 	Game_AudioManager.play_cave_level_boss_outro()	
@@ -84,12 +69,15 @@ func do_explosion(explosionNumber:int) -> void:
 func do_grab_talisman() -> void:
 	animation_player.play("grab_talisman")
 	
+func trigger_talisman() -> void:
+	demon_seal.grab_seal()
+	
 func do_ending() -> void:
 	yield(get_tree().create_timer(1), "timeout")
 	show_text()
-	player.celebrate()
+	player_for_cutscene.animation = "celebrate"
 	Game_AudioManager.stop_bgm()
-	yield(get_tree().create_timer(2), "timeout")
+	yield(get_tree().create_timer(2.5), "timeout")
 	fade_screen.go_to_scene("res://src/UI/WorldSelectScreen/WorldSelect.tscn")
 		
 		
@@ -98,31 +86,6 @@ func show_text() -> void:
 	
 func hover_text() -> void:
 	text_animation_player.play("hover")
-
-func move_player_right() -> void:
-	_move_player_right = true
-	_move_player_left = false
-	
-func move_player_left() -> void:
-	_move_player_left = true
-	_move_player_right = false
-	
-func move_player_stop() -> void:
-	_move_player_left = false
-	_move_player_right = false
-	
-func look_player_left() -> void:
-	move_player_left()
-	yield(get_tree().create_timer(0.05), "timeout")
-	move_player_stop()	
 		
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
 	pass
-	
-# This Stop Point Area ensures that the player stops at the exact spot
-# This is required because on HTML5 build it can walk further and therefore
-# be off the screen when player collects the gem
-func _on_StopPointArea2D_body_entered(body: Node) -> void:
-	if body.is_in_group(Constants.GROUP_PLAYER):
-		move_player_stop()
-		$StopPointArea2D/CollisionShape2D.set_deferred("disabled", true)
